66ae4b93e1e0b34b4091d090321c6c302faad6bc
Giridhar Balachandran <giridhar@zamp.finance>
2025-02-26 15:09:50 +0530

    31 SDK

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zamp_public_workflow_sdk',
-    version='0.0.30',    
+    version='0.0.31',    
     description='Workflow Manager',
     url='https://github.com/zamp-engineering/workflow_manager',
     author='Zamp Engineering',
