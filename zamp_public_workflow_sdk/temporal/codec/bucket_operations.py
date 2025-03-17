0746c4e50081d68e8a8272b0df2ae510991a3bbc
Giridhar Balachandran <giridhar@zamp.finance>
2025-03-17 14:48:13 +0530

    Async

--- zamp_public_workflow_sdk/temporal/codec/bucket_operations.py
+++ zamp_public_workflow_sdk/temporal/codec/bucket_operations.py
@@ -3,11 +3,11 @@ from google.cloud import storage
 
 class StorageClient(ABC):
     @abstractmethod
-    def upload_file(self, object_name: str, data: bytes | str) -> None:
+    async def upload_file(self, object_name: str, data: bytes | str) -> None:
         pass
 
     @abstractmethod
-    def get_file(self, object_name: str) -> bytes:
+    async def get_file(self, object_name: str) -> bytes:
         pass
 
 class GCSClient(StorageClient):
@@ -15,8 +15,8 @@ class GCSClient(StorageClient):
         self.client = storage.Client(project=project_id)
         self.bucket = self.client.bucket(bucket_name)
 
-    def upload_file(self, object_name: str, data: bytes | str) -> None:
+    async def upload_file(self, object_name: str, data: bytes | str) -> None:
         self.bucket.blob(object_name).upload_from_string(data)
 
-    def get_file(self, object_name: str) -> bytes:
+    async def get_file(self, object_name: str) -> bytes:
         return self.bucket.blob(object_name).download_as_bytes()
