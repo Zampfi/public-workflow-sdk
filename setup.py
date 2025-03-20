54f05a9921c38a60dbfa59090f3818c743587420
Giridhar Balachandran <giridhar@zamp.finance>
2025-03-21 02:51:40 +0530

    Moving to 54 for sentry and other issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zamp_public_workflow_sdk',
-    version='0.0.53',    
+    version='0.0.54',    
     description='Workflow Manager',
     url='https://github.com/zamp-engineering/workflow_manager',
     author='Zamp Engineering',
@@ -21,7 +21,6 @@ setup(
     install_requires=[
         'pydantic',
         'temporalio',
-        'sentry-sdk',
         'python-dateutil',
         'python-dotenv'
     ],
