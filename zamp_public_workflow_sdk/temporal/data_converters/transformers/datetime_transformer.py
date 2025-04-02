2676f155b8c62a7b1b6bd68a4abbd5a0bb55bed9
Giridhar Balachandran <giridhar@zamp.finance>
2025-04-02 13:09:17 +0530

    Minor hack to support datetime

--- zamp_public_workflow_sdk/temporal/data_converters/transformers/datetime_transformer.py
+++ zamp_public_workflow_sdk/temporal/data_converters/transformers/datetime_transformer.py
@@ -1,16 +1,15 @@
 from temporalio import workflow
-with workflow.unsafe.imports_passed_through():
-    from zamp_public_workflow_sdk.temporal.data_converters.transformers.base import BaseTransformer
-    from zamp_public_workflow_sdk.temporal.data_converters.transformers.models import GenericSerializedValue
-    from datetime import datetime, date
-    from typing import Any, Callable
-    from zamp_public_workflow_sdk.temporal.data_converters.type_utils import get_fqn
+from zamp_public_workflow_sdk.temporal.data_converters.transformers.base import BaseTransformer
+from zamp_public_workflow_sdk.temporal.data_converters.transformers.models import GenericSerializedValue
+from datetime import datetime, date
+from typing import Any, Callable
+from zamp_public_workflow_sdk.temporal.data_converters.type_utils import get_fqn
 
 class DateTransformer(BaseTransformer):
     def __init__(self):
         super().__init__()
         self.should_serialize: Callable[[Any], bool] = lambda value: isinstance(value, datetime) or isinstance(value, date)
-        self.should_deserialize: Callable[[Any, Any], bool] = lambda value, type_hint: type_hint is datetime or type_hint is date
+        self.should_deserialize: Callable[[Any, Any], bool] = lambda value, type_hint: issubclass(type_hint, datetime) or issubclass(type_hint, date)
 
     def _serialize_internal(self, value: Any) -> Any:
         return GenericSerializedValue(
@@ -19,7 +18,7 @@ class DateTransformer(BaseTransformer):
         )
     
     def _deserialize_internal(self, value: Any, type_hint: Any) -> datetime:
-        if type_hint is datetime:
+        if issubclass(type_hint, datetime):
             return datetime.fromisoformat(value)
         
         return date.fromisoformat(value)
