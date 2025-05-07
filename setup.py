ff8fe1e491a686df95d2e4d6b16c0c526b5df0fc
Giridhar Balachandran <giridhar@zamp.finance>
2025-05-07 18:37:13 +0530

    Minor changes to get logs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zamp_public_workflow_sdk',
-    version='0.0.64',    
+    version='0.0.75',    
     description='Workflow Manager',
     url='https://github.com/zamp-engineering/workflow_manager',
     author='Zamp Engineering',
@@ -20,7 +20,7 @@ setup(
     ],
     install_requires=[
         'pydantic',
-        'temporalio',
+        'zamp_temporal_sdk',
         'python-dateutil',
         'python-dotenv',
         'sentry-sdk'
