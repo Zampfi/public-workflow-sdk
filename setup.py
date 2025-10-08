c7ba29fd1a7e77720aab7a903ded738fb2f66824
Kuntal Bar <kuntal@Kuntals-MacBook-Pro.local>
2025-10-08 14:42:55 +0530

    fixed args

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zamp_public_workflow_sdk',
-    version='0.0.122',
+    version='0.0.123',
     description='Workflow Manager',
     url='https://github.com/zamp-engineering/workflow_manager',
     author='Zamp Engineering',
